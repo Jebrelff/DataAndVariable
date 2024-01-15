eb2f7ac95fa1efafe4b41eaa16bcd8d24972e5a2
Jebrelff <jebrelff@gmail.com>
2024-01-15 18:20:47 -0500

    "Create Variable"

--- DataType.js
+++ DataType.js
@@ -1,1 +1,12 @@
-console.log("Hi, How are you ?");
+
+let NameOfTheSpaceShuttle = 'determine';
+let ShuttleSpeedmph =	17500;
+let DistanceToMarskm =	225000000;
+let DistanceToTheMoonkm =	384400;
+const MilesPerkilometer =	0.621;
+
+console.log(typeof NameOfTheSpaceShuttle);
+console.log(typeof ShuttleSpeedmph);
+console.log(typeof DistanceToTheMoonkm);
+console.log(typeof DistanceToTheMoonkm);
+console.log(typeof MilesPerkilometer);
